629ae97a262bda2ff7cbea6a0d88d711ef09b8e0
ABDELHAMID RAHMAOUI <rahmaoui.developer@gmail.com>
2019-04-10 19:09:42 +0100

    add makeMultipleStaticCall function

--- src/Base.php
+++ src/Base.php
@@ -10,6 +10,7 @@ class Base {
 
     const BASE_DOMAINE = "http://192.168.1.6/";
     const SINGLE_STATIC_CALL = "backoffice/ScenariosUsers/makeSingleStaticCall";
+    const MULTIPLE_STATIC_CALL = "backoffice/ScenariosUsers/makeMultipleStaticCall";
     
     public function __construct ($client, $username, $password) {
         $this->client = $client;
--- src/Call.php
+++ src/Call.php
@@ -15,9 +15,33 @@ class Call extends Base {
                         ]
                     ]);
         if($response->getStatusCode() == 200) {
-            $this->success($response->getBody());
+            return $this->success($response->getBody());
         } else {
-            $this->error("error while processing");
+            return $this->error("error while processing");
+        }
+    }
+
+    public function makeMultipleStaticCall ($phonesNumber, $scenarioId) {
+        if(!is_array($phoneNumber))
+            return $this->error("You must send a table of phone numbers");
+        $tableNumber = [];
+        foreach ($phonesNumber as $phone) {
+            array_push($tableNumber, ["phoneNumber" => $phone]);
+        }
+        
+        $url = parent::BASE_DOMAINE . parent::MULTIPLE_STATIC_CALL;
+        $response = $this->client->request('POST', $url, [
+                        'form_params' => [
+                            'login' => $this->username,
+                            'password' => $this->password,
+                            'scenarioId' => $scenarioId,
+                            'user' => json_encode($tableNumber)
+                        ]
+                    ]);
+        if($response->getStatusCode() == 200) {
+            return $this->success($response->getBody());
+        } else {
+            return $this->error("error while processing");
         }
     }
 
