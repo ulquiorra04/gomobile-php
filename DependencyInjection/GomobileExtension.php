5c1b7e6872f0254883e70b5dff58ed3793c2fe80
ABDELHAMID RAHMAOUI <rahmaoui.developer@gmail.com>
2020-02-24 18:02:52 +0100

    add support of parameters

--- DependencyInjection/GomobileExtension.php
+++ DependencyInjection/GomobileExtension.php
@@ -4,6 +4,8 @@ namespace Gomobile\GomobileBundle\DependencyInjection;
 
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Loader;
 
 class GomobileExtension extends Extension
 {
@@ -12,5 +14,9 @@ class GomobileExtension extends Extension
 		$configuration = new Configuration ();
 
 		$this->processConfiguration($configuration, $configs);
+
+		// Load service file
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader->load('services.yaml');
 	}
 }
