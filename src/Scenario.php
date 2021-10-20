72c63c962a9a561b86e42b974703e3567cb8b6ad
ABDELHAMID RAHMAOUI <rahmaoui.developer@gmail.com>
2021-10-20 08:33:56 +0100

    add support of update audio scenario

--- src/Scenario.php
+++ src/Scenario.php
@@ -4,15 +4,22 @@ namespace Gomobile\GomobileBundle\src;
 
 class Scenario extends Base {
 
-	/**
+    private $url;
+
+    public function __construct($client, $username, $password, $demo = false)
+    {
+        $this->url = ($demo) ? parent::BASE_LOCAL_DOMAINE : parent::BASE_GLOBAL_DOMAINE;
+        parent::__construct($client, $username, $password, $demo);
+    }
+
+    /**
 	 * GET List of scenarios
 	 * 
 	 * @return Array of scenarios
 	 */
 	public function getScenarios () {
-		$url = ($this->demo) ? parent::BASE_LOCAL_DOMAINE : parent::BASE_GLOBAL_DOMAINE;
-		$url .= parent::SCENARIO_LIST;
-		$response = $this->client->request('POST', $url, [
+		$this->url .= parent::SCENARIO_LIST;
+		$response = $this->client->request('POST', $this->url, [
 						'form_params' => [
 							'login' => $this->username,
 							'password' => $this->password
@@ -36,9 +43,8 @@ class Scenario extends Base {
 	 * @return Object Scenario
 	 */
 	public function getScenario ($scenarioId) {
-		$url = ($this->demo) ? parent::BASE_LOCAL_DOMAINE : parent::BASE_GLOBAL_DOMAINE;
-		$url .= parent::SCENARIO_SINGLE;
-		$response = $this->client->request('POST', $url, [
+		$this->url .= parent::SCENARIO_SINGLE;
+		$response = $this->client->request('POST', $this->url, [
 						'form_params' => [
 							'login' => $this->username,
 							'password' => $this->password,
@@ -63,9 +69,8 @@ class Scenario extends Base {
 	 * @return int scenarioId
 	 */
 	public function addScenario ($name) {
-		$url = ($this->demo) ? parent::BASE_LOCAL_DOMAINE : parent::BASE_GLOBAL_DOMAINE;
-		$url .= parent::SCENARIO_ADD;
-		$response = $this->client->request('POST', $url, [
+		$this->url .= parent::SCENARIO_ADD;
+		$response = $this->client->request('POST', $this->url, [
 							'form_params' => [
 								'login' => $this->username,
 								'password' => $this->password,
@@ -84,9 +89,8 @@ class Scenario extends Base {
 	}
 
 	public function getAudiosScenario ($scenarioId) {
-		$url = ($this->demo) ? parent::BASE_LOCAL_DOMAINE : parent::BASE_GLOBAL_DOMAINE;
-		$url .= parent::SCENARIO_AUDIO_LIST;
-		$response = $this->client->request('POST', $url, [
+		$this->url .= parent::SCENARIO_AUDIO_LIST;
+		$response = $this->client->request('POST', $this->url, [
 							'form_params' => [
 								'login' => $this->username,
 								'password' => $this->password,
@@ -104,6 +108,13 @@ class Scenario extends Base {
 		}
 	}
 
+    /**
+     * @param $name
+     * @param $order
+     * @param $file_path
+     * @param $scenarioId
+     * @return array
+     */
 	public function addSimpleAudioScenario ($name, $order, $file_path, $scenarioId) {
 		if(file_exists($file_path))
 			return $this->addAudioScenario($name, 1, $order, $file_path, $scenarioId);
@@ -111,12 +122,33 @@ class Scenario extends Base {
 			return $this->error("File not found");
 	}
 
+    /**
+     * @param $name
+     * @param $order
+     * @param $file_path
+     * @param $scenarioId
+     * @return array
+     */
 	public function addInteractiveAudioScenario ($name, $order, $file_path, $scenarioId) {
 		if(file_exists($file_path))
 			return $this->addAudioScenario($name, 159, $order, $file_path, $scenarioId);
 		else
 			return $this->error("File not found");
 	}
+
+    /**
+     * @param $name
+     * @param $order
+     * @param $file_path
+     * @param $scenarioId
+     * @return array
+     */
+	public function updateInteractiveAudioScenario ($name, $order, $file_path, $scenarioId) {
+        if(file_exists($file_path))
+            return $this->updateAudioScenario($name, 159, $order, $file_path, $scenarioId);
+        else
+            return $this->error("File not found");
+    }
 
 	/**
 	 * ADD Dynamic Audio to Scenario
@@ -140,9 +172,8 @@ class Scenario extends Base {
 	}
 
 	private function addAudioScenario ($name, $type, $order, $file_path, $scenario, $voiceId = 0, $metadata = 0) {
-		$url = ($this->demo) ? parent::BASE_LOCAL_DOMAINE : parent::BASE_GLOBAL_DOMAINE;
-		$url .= parent::SCENARIO_AUDIO_ADD;
-		$response = $this->client->request('POST', $url, [
+		$this->url .= parent::SCENARIO_AUDIO_ADD;
+		$response = $this->client->request('POST', $this->url, [
 							'multipart' => [
 							    ['name' => 'login', 'contents' => $this->username],
                                 ['name' => 'password', 'contents' => $this->password],
@@ -169,5 +200,33 @@ class Scenario extends Base {
 		}
 	}
 
-
+    private function updateAudioScenario ($name, $type, $order, $file_path, $scenario, $voiceId = 0, $metadata = 0)
+    {
+        $this->url .= parent::SCENARIO_AUDIO_EDIT;
+        $response = $this->client->request('POST', $this->url, [
+            'multipart' => [
+                ['name' => 'login', 'contents' => $this->username],
+                ['name' => 'password', 'contents' => $this->password],
+                ['name' => 'scenarioId', 'contents' => $scenario],
+                ['name' => 'name', 'contents' => $name],
+                ['name' => 'type', 'contents' => $type],
+                ['name' => 'order', 'contents' => $order],
+                ['name' => 'voice', 'contents' => $voiceId],
+                ['name' => 'metadata', 'contents' => $metadata],
+                [
+                    'name' => 'audio_file',
+                    'contents' => fopen($file_path, "r")
+                ]
+            ]
+        ]);
+        if($response->getStatusCode() == 200) {
+            $result = json_decode($response->getBody()->getContents());
+            if($result->status == 1)
+                return $this->success($result->message, $result->data);
+            else
+                return $this->error($result->message);
+        } else {
+            return $this->error("error while processing");
+        }
+    }
 }
